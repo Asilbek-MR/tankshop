cd31bf4afcb759cd997832acf639b9d57c066507
Asilbek-MR <asilbekdavid@gmail.com>
2023-12-21 23:55:18 +0500

    remove secretkey

--- utility/instagram_graph_api/defines_py3.py
+++ utility/instagram_graph_api/defines_py3.py
@@ -10,11 +10,11 @@ def getCreds() :
 	"""
 
 	creds = dict() # dictionary to hold everything
-	creds['access_token'] = 'EAAPPMvkMZBsoBOZCoL6DSrCvhzyVSI9aLeQW6OHJkjUtcm0A0xEIFrhTQauiVOrdXKS5z2ZAZAV6ayJIOaFfJ6aAstgr8LwXyAdiPGrlEZA4LgmC4or8syWZA5JkoygAd5nEtX2nMM6JnS2IkQLy4v7yYrXXUzOLI8Hm1Hlex5ikEioNNGgDbZAFinYIaOC6bJhU8ygWs6GI8oZBFPb0HeyAvZBcakI0ZD' # access token for use with all api calls
+	creds['access_token'] = '' # access token for use with all api calls
 	creds['graph_domain'] = 'https://graph.facebook.com/' # base domain for api calls
 	creds['graph_version'] = 'v6.0' # version of the api we are hitting
 	creds['endpoint_base'] = creds['graph_domain'] + creds['graph_version'] + '/' # base endpoint with domain and version
-	creds['instagram_account_id'] = '17841448219410247' # users instagram account id
+	creds['instagram_account_id'] = '' # users instagram account id
 
 	return creds
 
